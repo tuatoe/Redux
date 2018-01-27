ab4f01a41777baa70fcdcbfe2f3c463bbd8af1cc
Tuatoe <tuatoe.daynuah@gmail.com>
2018-01-26 23:49:33 -0600

    react-redux app initial build setup

---
+++ learn-redux/client/components/Main.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Link} from 'react-router';
+
+
+const Main = React.createClass({
+    render(){
+        return(
+            <div>
+                <h1>
+                    <Link to="/">Reduxstagram</Link>
+                    {React.cloneElement(this.props.children, this.props)}
+                </h1>
+            </div>
+        );
+    }
+})
+
+export default Main;
---
+++ learn-redux/client/store.js
@@ -0,0 +1,21 @@
+import { createStore, compse} from 'redux';
+import {syncHistoryWithStore} from 'react-router-redux';
+import {browserHistory} from 'react-router';
+
+//import the root reducer
+import rootReducer from './reducers/index';
+import comments from './reducers/comments';
+import posts from './reducers/posts';
+
+//create an object for the default data
+
+const defaultState = {
+    posts,
+    comments
+};
+
+const store = createStore(rootReducer, defaultState);
+
+export const history = syncHistoryWithStore(browserHistory, store);
+
+export default store;
